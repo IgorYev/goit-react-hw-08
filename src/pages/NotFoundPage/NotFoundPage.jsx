886d33bca17c0ad0cbfda7c430a7f5c2b7e5c412
IgorYev <123358377+IgorYev@users.noreply.github.com>
2024-05-29 14:52:15 +0300

    final2

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,19 +1,14 @@
 import { Link } from "react-router-dom";
 import PageTitle from "../../components/PageTitle/PageTitle";
-import TransferWithinAStationIcon from "@mui/icons-material/TransferWithinAStation";
-import css from "./NotFoundPage.module.css";
 
 export default function NotFoundPage() {
   return (
-    <div className={css.container}>
+    <div>
       <PageTitle>Opps! Page not found! Sorry!</PageTitle>
-      <p className={css.message}>
+      <p>
         Please, visit
-        <Link className={css.link} to="/">
-          home page
-        </Link>
+        <Link to="/">home page</Link>
       </p>
-      <TransferWithinAStationIcon className={css.icon} sx={{ fontSize: 60 }} />
     </div>
   );
 }
